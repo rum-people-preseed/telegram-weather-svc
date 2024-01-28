d6a183ad88f54c2d43111c1899570a6a5e311ca0
statnik <ext-mykhailo.statnik@here.com>
2024-01-28 16:06:24 +0200

    chore: added basic bot example

--- go.mod
+++ go.mod
@@ -2,3 +2,10 @@ module github.com/rum-people-preseed/telegram-weather-svc
 
 go 1.19
 
+require go.uber.org/zap v1.26.0
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"go.uber.org/zap"
+	"os"
+)
 
+func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+
+	telegramApiToken := os.Getenv("TELEGRAM_API_TOKEN")
+	if telegramApiToken == "" {
+		sugar.Error("Failed to load env TELEGRAM_API_TOKEN")
+		os.Exit(1)
+	}
+
+	apiBot, err := tgbotapi.NewBotAPI(telegramApiToken)
+	if err != nil {
+		sugar.Error("Failed to bind to API bot with token")
+		os.Exit(1)
+	}
+
+	apiBot.Debug = true
+
+	sugar.Info("Authorized on account %s", apiBot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := apiBot.GetUpdatesChan(u)
+
+	if err != nil {
+		sugar.Error("Failed to get updates chanel")
+		os.Exit(1)
+	}
+
+	for update := range updates {
+		if update.Message == nil { // ignore non-Message updates
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+		if _, err := apiBot.Send(msg); err != nil {
+			sugar.Error("Failed to send message")
+			os.Exit(1)
+		}
+	}
 }
